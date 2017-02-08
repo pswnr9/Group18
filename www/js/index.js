25df6117112406e864e127b6b72ba72fe8e1bac4
bobscoe <mjg5kc@mail.missouri.edu>
2017-02-07 23:03:27 -0600

    update app.js back to version provided by azure; other minor changes

--- www/js/index.js
+++ www/js/index.js
@@ -20,6 +20,7 @@ var app = {
     // Application Constructor
     initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+        //client = new WindowsAzure.MobileServiceClient('http://pockethsamanager.azurewebsites.net-fail');
     },
 
     // deviceready Event Handler
